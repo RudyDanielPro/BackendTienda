48ba099465d00eed61f673e93b27a6362f7e7931
Rudy <rudydanielcarballo@gmail.com>
2026-02-14 12:11:58 -0500

    corrigiendo envios

--- src/main/java/com/gestion/clientes/Servicios/EmailService.java
+++ src/main/java/com/gestion/clientes/Servicios/EmailService.java
@@ -13,11 +13,18 @@ public class EmailService {
 
     public void enviarNotificacionAdmin(String listaProd, String links) {
         SimpleMailMessage email = new SimpleMailMessage();
-        email.setTo("rudydaniel050526@gmail.com"); // TU CORREO
+        
+        // -----------------------------------------------------------
+        // CAMBIO AQUÍ: Pon el correo de la persona que debe RECIBIR el aviso
+        // No pongas el mismo correo desde el que se envía.
+        // -----------------------------------------------------------
+        email.setTo("rudydanielcarballo@gmail.com"); 
+        
         email.setSubject("🛍️ Nuevo Pedido - Confirmación de Stock Requerida");
         email.setText("Hola Admin,\n\nSe ha iniciado un pedido por WhatsApp.\n\n" +
                       "PRODUCTOS:\n" + listaProd + "\n\n" +
                       "⚠️ PARA DESCONTAR EL STOCK DE ESTA VENTA, HAZ CLIC EN LOS LINKS:\n\n" + links);
+        
         mailSender.send(email);
     }
 }
